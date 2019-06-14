eb69f7c842f68e69e49bbced8d464c8679236a45
trynyen <tryn.yen@gmail.com>
2019-06-13 20:50:54 -0500

    adjusted time and firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,15 +8,36 @@ $("#submit").click(function(event){
     var destinationInput = $("#destinationInput").val().trim();
     var frequencyInput = $("#frequencyInput").val().trim();
     var firstTrainTime = $("#firstTrainTime").val().trim();
-    var nextArrivalInput = moment(firstTrainTime,"HH:mm").format("LT")
-    // var minutesAway = moment().diff(moment(firstTrainTime,"HH:mm"))
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+
+    // Difference between the times
+    var diffTime = currentTime.diff(moment(firstTimeConverted), "minutes");
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % frequencyInput;
+    
+    // Minute Until Train
+    var minutesAway = frequencyInput - tRemainder;
+
+    // Next Train & changed time format
+    var nextTrainInput = moment().add(minutesAway, "minutes");
+    var nextTrain = moment(nextTrainInput).format("hh:mm A");
 
     var newTrain = {
         trainName: trainNameInput,
         destination: destinationInput,
         frequency: frequencyInput,
-        nextArrival: nextArrivalInput,
+        nextTrainInput: nextTrain,
+        minutesAwayInput: minutesAway,
+
     }
+
     // Code for "Setting values in the database"
     database.ref().push(newTrain);
 
@@ -32,18 +53,11 @@ database.ref().on("child_added", function(childSnapshot) {
     var trainNameInput = childSnapshot.val().trainName;
     var destinationInput = childSnapshot.val().destination;
     var frequencyInput = childSnapshot.val().frequency;
-    var nextArrivalInput = childSnapshot.val().nextArrival;
-
+    var nextTrain = childSnapshot.val().nextTrainInput;
+    var minutesAway = childSnapshot.val().minutesAwayInput;
     
     // Create the new row
-    var newRow = $("<tr>").append(
-        $("<td>").text(trainNameInput),
-        $("<td>").text(destinationInput),
-        $("<td>").text(frequencyInput),
-        $("<td>").text(nextArrivalInput),
-
-    );
-    $("#trainTable > tbody").append(newRow);
+    $("#trainTable > tbody").append("<tr><td>" + trainNameInput + "</td><td>" + destinationInput + "</td><td>" + frequencyInput + "</td><td>" + nextTrain + "</td><td>" + minutesAway + "</td></tr>");
 
 
 })
